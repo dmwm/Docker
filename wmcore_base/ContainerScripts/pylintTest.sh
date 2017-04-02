7a3435b6f18099269d683f151d4e3ae18be6a76e
Eric Vaandering <ewv@nakuru.dyndns.cern.ch>
2017-04-02 18:38:54 +0200

    Comment and clean up

--- wmcore_base/ContainerScripts/pylintTest.sh
+++ wmcore_base/ContainerScripts/pylintTest.sh
@@ -5,53 +5,40 @@ if [ -z "$ghprbPullId" -o -z "$ghprbTargetBranch" ]; then
   exit 1
 fi
 
+# Setup the environment
 source ./env_unittest.sh
-
 pushd wmcore_unittest/WMCore
 export PYTHONPATH=`pwd`/test/python:`pwd`/src/python:$PYTHONPATH
 
-# debug
-set -x
-
 # Figure out the one commit we are interested in and what happens to the repo if we were to merge it
-
-#git fetch --tags  https://github.com/dmwm/WMCore.git "+refs/heads/*:refs/remotes/origin/*"
 git config remote.origin.url https://github.com/dmwm/WMCore.git
-#git config --add remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-
 git fetch origin pull/${ghprbPullId}/merge:PR_MERGE
 export COMMIT=`git rev-parse "PR_MERGE^{commit}"`
 git checkout ${ghprbTargetBranch}
-#git pull
+git pull
+
+# Which python files changed?
 git diff --name-only  ${ghprbTargetBranch}..${COMMIT} > allChangedFiles.txt
-
-# Debug
-cat allChangedFiles.txt
-
 ${HOME}/ContainerScripts/IdentifyPythonFiles.py allChangedFiles.txt > changedFiles.txt
 
-# Debug
-cat changedFiles.txt
-echo "{}" > pylintReport.json
-
+# Get pylint report for master
 git checkout -f $ghprbTargetBranch
+echo "{}" > pylintReport.json
 while read name; do
   pylint --evaluation='10.0 - ((float(5 * error + warning) / statement) * 10)'  --rcfile standards/.pylintrc --msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'  $name > pylint.out || true
  ${HOME}/ContainerScripts/AggregatePylint.py base
-
 done <changedFiles.txt
 
-
+# Get pylint report for the tip of our branch
 git checkout -f $COMMIT
 while read name; do
   pylint --evaluation='10.0 - ((float(5 * error + warning) / statement) * 10)'  --rcfile standards/.pylintrc  --msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'  $name  > pylint.out || true
  ${HOME}/ContainerScripts/AggregatePylint.py test
-
 done <changedFiles.txt
 
-ls -lR
+# Save the artifacts to a directory shared by the container and the node
 cp *.json ${HOME}/artifacts/
-ls -lR ${HOME}
+
 popd
 
 
