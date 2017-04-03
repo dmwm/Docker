5506889b37149039516c08ded2028a6bd7937a4e
Eric Vaandering <ewv@nakuru.dyndns.cern.ch>
2017-04-03 10:06:54 +0200

    Remove debug

--- wmcore_base/ContainerScripts/pyfutureTest.sh
+++ wmcore_base/ContainerScripts/pyfutureTest.sh
@@ -13,7 +13,7 @@ export PYTHONPATH=`pwd`/test/python:`pwd`/src/python:${PYTHONPATH}
 git config remote.origin.url https://github.com/dmwm/WMCore.git
 git fetch origin pull/${ghprbPullId}/merge:PR_MERGE
 export COMMIT=`git rev-parse "PR_MERGE^{commit}"`
-git checkout -f $COMMIT
+git checkout -f ${COMMIT}
 
 futurize -1 src/ test/ > test.patch
 
@@ -21,12 +21,8 @@ futurize -1 src/ test/ > test.patch
 git diff --name-only  ${ghprbTargetBranch}..${COMMIT} > changedFiles.txt
 git diff-tree --name-status  -r ${ghprbTargetBranch}..${COMMIT} | egrep "^A" | cut -f 2 > addedFiles.txt
 
-# Debug
-cat changedFiles.txt
-cat addedFiles.txt
-
 while read name; do
-  futurize -f idioms  $name  >> idioms.patch || true
+  futurize -f idioms $name  >> idioms.patch || true
 done <changedFiles.txt
 
 ${HOME}/ContainerScripts/AnalyzePyFuture.py > added.message
@@ -34,5 +30,3 @@ ${HOME}/ContainerScripts/AnalyzePyFuture.py > added.message
 cp test.patch idioms.patch added.message ${HOME}/artifacts/
 
 popd
-
-
