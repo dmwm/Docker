c83b6e483bd281bd291ca4e55e57245ed12ab2ea
Eric Vaandering <ewv@nakuru.dyndns.cern.ch>
2017-04-04 09:29:00 +0200

    Reorganize and clean up various scripts
    Separate the ones from testing from the ones used in the container creation.
    
    Still work to do here.

--- wmcore_base/ContainerScripts/installWMCore.sh
+++ wmcore_base/ContainerScripts/installWMCore.sh
@@ -5,6 +5,7 @@ curl https://raw.githubusercontent.com/dmwm/WMCore/master/test/deploy/deploy_uni
 chmod +x /home/dmwm/ContainerScripts/deploy_unittest.sh
 sh /home/dmwm/ContainerScripts/deploy_unittest.sh
 
+echo "export PYTHONPATH=/home/dmwm/wmcore_unittest/WMCore/src/python:\$PYTHONPATH" >> ./env_unittest.sh
 # Shut down services so the docker container doesn't have stale PID & socket files
 source ./env_unittest.sh
 $manage stop-services
