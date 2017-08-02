b20802636ff94920da5442665a92c8fab5f31a2e
Eric Vaandering <ericvaandering@gmail.com>
2017-08-02 13:00:52 -0500

    pylint and pylint3 for whole code base

--- crab_tests/CRABScripts/pylintCRAB.sh
+++ crab_tests/CRABScripts/pylintCRAB.sh
@@ -4,22 +4,55 @@
 
 # Setup the environment
 source ./env_unittest.sh
-pushd dbs_test/DBS
+
+# Do CRABServer code
+
+pushd crabserver_test/CRABServer
 
 timeout -s 9 5m git checkout master || timeout -s 9 5m git checkout master
 timeout -s 9 5m git pull origin || timeout -s 9 5m git pull origin
 
 # Run pylint on the whole code base
-# Remove C0103 for now since all dbs modules start with dbs instead of Dbs. Maybe a new standards file later.
-export PYTHONPATH=`pwd`/Server/Python/src:`pwd`/Client/src/python/dbs/api:${PYTHONPATH}
-pylint --rcfile /home/dmwm/dbs_test/.pylintrc -d C0103 -f parseable Client/src/python/dbs
-pylint --rcfile /home/dmwm/dbs_test/.pylintrc -d C0103 -f parseable Server/Python/src/dbs
-pylint --rcfile /home/dmwm/dbs_test/.pylintrc -d C0103 -f parseable PycurlClient/src/python/RestClient
+export PYTHONPATH=`pwd`/test/python:`pwd`/src/python:${PYTHONPATH}
+popd
+
+# Do CRABClient code
+pushd crabclient_test/CRABClient
+
+timeout -s 9 5m git checkout master || timeout -s 9 5m git checkout master
+timeout -s 9 5m git pull origin || timeout -s 9 5m git pull origin
+
+# Run pylint on the whole code base
+export PYTHONPATH=`pwd`/test/python:`pwd`/src/python:${PYTHONPATH}
+popd
+
+# Do ASO code
+
+pushd aso_test/AsyncStageout
+
+timeout -s 9 5m git checkout master || timeout -s 9 5m git checkout master
+timeout -s 9 5m git pull origin || timeout -s 9 5m git pull origin
+
+# Run pylint on the whole code base
+export PYTHONPATH=`pwd`/test/python:`pwd`/src/python:${PYTHONPATH}
+popd
+
+export MODULES="aso_test/AsyncStageout/test/python/AsyncStageOut_t/ aso_test/AsyncStageout/src/python/AsyncStageOut " \
+ "crabserver_test/CRABServer/src/python/UserFileCache crabserver_test/CRABServer/src/python/taskbuffer " \
+ "crabserver_test/CRABServer/src/python/TaskWorker crabserver_test/CRABServer/src/python/CRABInterface " \
+ "crabserver_test/CRABServer/src/python/Databases crabclient_test/CRABClient/src/python/CRABClient " \
+ "crabclient_test/CRABClient/src/python/CRABAPI "
+
+pylint --rcfile /home/dmwm/crabclient_test/.pylintrc -f parseable $MODULES
+
 
 # Fix pep8 which has the wrong python executable
-echo "#! /usr/bin/env python" > ../pep8
-cat `which pep8` >> ../pep8
-chmod +x ../pep8
+echo "#! /usr/bin/env python" > ./pep8
+cat `which pep8` >> ./pep8
+chmod +x ./pep8
 
 # Run PEP-8 checker but not in pylint format
-../pep8 --format=default --exclude=.svn,CVS,.bzr,.hg,.git,__pycache__,.tox. Client/src/python/dbs/ Server/Python/src/dbs/ PycurlClient/src/python/RestClient/
+./pep8 --format=default --exclude=.svn,CVS,.bzr,.hg,.git,__pycache__,.tox. crabserver_test crabclient_test aso_test
+
+exit
+
