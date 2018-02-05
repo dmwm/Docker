edbb239e35a187d07950ea316634bd14f21be811
Alan Malta Rodrigues <alan.malta@cern.ch>
2018-02-06 00:25:48 +0100

    Only run futurize on python files; removed a couple of fixers
    Apply Erics comments
    
    futurize 1st stage only on files that changed

--- wmcore_base/ContainerScripts/pyfutureTest.sh
+++ wmcore_base/ContainerScripts/pyfutureTest.sh
@@ -5,6 +5,7 @@ if [ -z "$ghprbPullId" -o -z "$ghprbTargetBranch" ]; then
   exit 1
 fi
 
+echo "$(TZ=GMT date): running pyfutureTest.sh script"
 source ./env_unittest.sh
 
 pushd wmcore_unittest/WMCore
@@ -15,19 +16,23 @@ git fetch origin pull/${ghprbPullId}/merge:PR_MERGE
 export COMMIT=`git rev-parse "PR_MERGE^{commit}"`
 git checkout -f ${COMMIT}
 
-futurize -1 src/ test/ > test.patch
-
-# Get changed files and analyze for idioms
-git diff --name-only  ${ghprbTargetBranch}..${COMMIT} > changedFiles.txt
+echo "$(TZ=GMT date): figuring out what are the files that changed"
+# Find all the changed files and filter python-only
+git diff --name-only  ${ghprbTargetBranch}..${COMMIT} > allChangedFiles.txt
+${HOME}/ContainerScripts/IdentifyPythonFiles.py allChangedFiles.txt > changedFiles.txt
 git diff-tree --name-status  -r ${ghprbTargetBranch}..${COMMIT} | egrep "^A" | cut -f 2 > addedFiles.txt
 
+echo "$(TZ=GMT date): running futurize 1st stage and some fixers"
 while read name; do
-  futurize -f execfile -f filter -f raw_input >> test.patch || true
+  futurize -1 $name >> test.patch
+  futurize -f execfile -f filter -f raw_input $name >> test.patch || true
   futurize -f idioms $name  >> idioms.patch || true
 done <changedFiles.txt
 
+# Get added files and analyze future imports
+echo "$(TZ=GMT date): running AnalyzePyFuture.py script"
 ${HOME}/ContainerScripts/AnalyzePyFuture.py > added.message
 
 cp test.patch idioms.patch added.message ${HOME}/artifacts/
-
+echo "$(TZ=GMT date): done with all tests and copying files over to artifacts!"
 popd
